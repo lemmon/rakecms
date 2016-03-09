29d9c1b8d632f6d34747d5d3f7406c4775421a07
Lemmon <lemmon@users.noreply.github.com>
2016-03-09 12:52:38 +0100

    sp extension / nbsp

--- src/Rake/Template/Extension.php
+++ src/Rake/Template/Extension.php
@@ -75,10 +75,11 @@ class Extension extends \Twig_Extension
                 $res = array_map(function($res) use ($x) {
                     return preg_replace_callback('/<[^>]*>(*SKIP)(*F)|[^<]+/um', function($m) use ($x) { // match anything but html tags
                         $res = $m[0];
-                        $res = preg_replace('/(\b\w{1,' .$x. '})\s(?=\w)/um', '$1&nbsp;', $res);       // nbsp
-                        $res = preg_replace('/"(.*)"/sumU', '&bdquo;$1&ldquo;', $res);      // quotes
-                        $res = preg_replace('/(?<!!)\-\-(?!\>)/', '&ndash;', $res);         // dashes
-                        $res = preg_replace('/\s*\.{3,}/', '&hellip;', $res);               // hellip
+                        $res = preg_replace('/\x{00a0}/u', '&nbsp;', $res);                         // nbsp
+                        $res = preg_replace('/(\b\w{1,' .$x. '})\s(?=\w)/um', '$1&nbsp;', $res);    // nbsp
+                        $res = preg_replace('/"(.*)"/sumU', '&bdquo;$1&ldquo;', $res);              // quotes
+                        $res = preg_replace('/(?<!!)\-\-(?!\>)/', '&ndash;', $res);                 // dashes
+                        $res = preg_replace('/\s*\.{3,}/', '&hellip;', $res);                       // hellip
                         return $res;
                     }, $res);
                 }, $res);
